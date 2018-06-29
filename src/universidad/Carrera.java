795dd0404d885be2c3d694a1d767e3f21418c40f
EmilioMedina <emilio.medinamartinez@hotmail.com>
2018-06-28 21:18:23 -0300

    V1.1 - Carrera File

---
+++ src/universidad/Carrera.java
@@ -0,0 +1,43 @@
+
+package universidad;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Carrera {
+    String listado = "";
+    
+    public String ListarCarreras(){
+        try {
+            String sqlListadoCarreras = "select * from Carrera";
+            Conexion objConexion = new Conexion();
+            Connection conn = objConexion.DevolverConexion();
+            
+            PreparedStatement objPreparedStatment =
+                    conn.prepareStatement(sqlListadoCarreras);
+            
+            ResultSet objResultSet = objPreparedStatment.executeQuery();
+            
+            while(objResultSet.next()){
+                listado = 
+                        listado + 
+                        objResultSet.getInt("idCarrera") +
+                        " " +
+                        objResultSet.getString("Nombre") +
+                        " (" + 
+                        objResultSet.getString("codigo")  +
+                        ")";
+                listado = listado + ", ";
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Carrera.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return listado;
+    }
+}
